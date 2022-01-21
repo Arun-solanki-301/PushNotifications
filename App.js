2554321183bf012cb1902e982565773ff001ac30
Arun solanki <arunsolanki@excellencetechnologies.in>
2022-01-21 17:44:37 +0530

    2nd commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import messaging from '@react-native-firebase/messaging';
+
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
+
+
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
